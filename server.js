03b9b8311dcbfdaf676d36b2a63a8e9afbabcc8c
siminaG <simina.ghe@yahoo.com>
2020-03-28 18:19:48 +0200

    solved error on build

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/e-health-lab'));
+app.use(express.static(__dirname + '/dist/eHealthLab'));
 
 app.get('/*', function(req,res) {
 
-  res.sendFile(path.join(__dirname+'/dist/e-health-lab/index.html'));
+  res.sendFile(path.join(__dirname+'/dist/eHealthLab/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
